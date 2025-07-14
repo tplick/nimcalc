0eba44cd94b48df7e542f942e31277b62c558d2c
Tom Plick <tomplick@gmail.com>
2025-07-13 21:32:37 -0400

    cram: initial version of bitfield code

--- cram.ml
+++ cram.ml
@@ -12,43 +12,47 @@ let over list fn = List.iter fn list
 
 
 
-type cram_game = {board: SqSet.t; is_new: bool; last_move: ((int * int) * (int * int)) option;
+type cram_game = {board: int array; is_new: bool; last_move: ((int * int) * (int * int)) option;
             height: int; width: int}
 
 let c_new_game a b =
-    let set = ref SqSet.empty
-    in
-    for i = 1 to a do
-        for j = 1 to b do
-            set := SqSet.add (i, j) !set
-        done
-    done;
-    {board = !set; is_new = true; last_move = None; height = a; width = b}
+    let board = Array.make a (1 lsl b - 1)
+    in {board = board; is_new = true; last_move = None; height = a; width = b}
 
 let can_move_horiz game (a, b) =
-    SqSet.mem (a, b+1) game.board
+    if b + 1 >= game.width
+        then false
+        else
+
+    let mask = 3 lsl b and
+        row = game.board.(a)
+    in row land mask = mask
 
 let can_move_vert  game (a, b) =
-    SqSet.mem (a+1, b) game.board
+    if a + 1 >= game.height
+        then false
+        else
+
+    let mask = 1 lsl b
+    in game.board.(a) land game.board.(a+1) land mask = mask
+
+let board_without game (a, b) (c, d) =
+    let new_board = Array.copy game.board
+    in  new_board.(a) <- new_board.(a) land lnot (1 lsl b);
+        new_board.(c) <- new_board.(c) land lnot (1 lsl d);
+        new_board
 
 let c_after_horiz_move game (a, b) =
   {game with
-     board = SqSet.filter (fun (c, d) -> a <> c || (d <> b && d <> b+1)) game.board;
+     board = board_without game (a, b) (a, b+1);
      is_new = false;
      last_move = Some ((a, b), (a, b+1))}
 
 let c_after_vert_move game (a, b) =
   {game with
-     board = SqSet.filter (fun (c, d) -> b <> d || (c <> a && c <> a+1)) game.board;
+     board = board_without game (a, b) (a+1, b);
      is_new = false;
      last_move = Some ((a, b), (a+1, b))}
-
-
-let rec list_of_set set =
-    let x = ref []
-    in
-    SqSet.iter (fun sq -> x := sq :: !x) set;
-    !x
 
 
 let get_last_move opt =
@@ -68,11 +72,22 @@ let rec faux_shuffle = function
     | x -> let y, z = split_list_alternating x [] []
            in (faux_shuffle y) @ (faux_shuffle z)
 
+let all_squares_on_board game =
+    let res = ref [] in
+    for r = 0 to game.height - 1 do
+        let row = game.board.(r) in
+        for c = 0 to game.width - 1 do
+            if row land (1 lsl c) > 0
+                then res := (r, c) :: !res
+        done
+    done;
+    List.rev !res
+
 let c_options_for_game game =
-    let full_square_list = list_of_set game.board
+    let full_square_list = all_squares_on_board game
     in
     let square_list = if game.is_new
-        then List.filter (fun (a, b) -> a <= (game.height+1) / 2 && b <= (game.width+1) / 2) full_square_list
+        then List.filter (fun (a, b) -> a <= (game.height - 1) / 2 && b <= (game.width - 1) / 2) full_square_list
         else full_square_list
     in
     let horiz_moves = List.filter (can_move_horiz game) square_list
@@ -102,6 +117,7 @@ let does_have_at_most_two_neighbors (a, b) set =
         if SqSet.mem sq set then incr count);
     !count <= 2
 
+(*
 let is_row_missing game r =
     let any = ref false in
     for c = 1 to game.width do
@@ -115,20 +131,31 @@ let is_column_missing game c =
         any := !any || SqSet.mem (r, c) game.board
     done;
     !any
+*)
+
+let make_set_from_board game =
+    let set = ref SqSet.empty in
+    for r = 0 to game.height - 1 do
+        let row = game.board.(r) in
+        for c = 0 to game.width - 1 do
+            if row land (1 lsl c) > 0
+                then set := SqSet.add (r, c) !set
+        done
+    done;
+    !set
 
 let should_try_to_split game =
-    if SqSet.cardinal game.board < 5 || game.last_move == None
+    let board_set = make_set_from_board game in
+
+    if SqSet.cardinal board_set < 5 || game.last_move == None
         then false
         else
 
     match game.last_move with
         | None -> false
         | Some ((a, b), (c, d)) ->
-            does_have_at_most_two_neighbors (a, b) game.board
-         || does_have_at_most_two_neighbors (c, d) game.board
-(*            is_row_missing game a || (a <> c && is_row_missing game c)
-         || is_column_missing game b || (b <> d && is_column_missing game d) *)
-
+            does_have_at_most_two_neighbors (a, b) board_set
+         || does_have_at_most_two_neighbors (c, d) board_set
 
 let rec set_map fn set =
     if SqSet.is_empty set
@@ -138,66 +165,54 @@ let rec set_map fn set =
             in
             SqSet.add (fn elt) (set_map fn (SqSet.remove elt set))
 
-let renormalize_game game =
-    if true then game else
-
-    let min_row = SqSet.fold (fun (r2, _) r -> min r r2) game.board 1000
-    and min_col = SqSet.fold (fun (_, c2) c -> min c c2) game.board 1000
-    and max_row = SqSet.fold (fun (r2, _) r -> max r r2) game.board 0
-    and max_col = SqSet.fold (fun (_, c2) c -> max c c2) game.board 0
-    in
-    {game with
-        board  = set_map (fun (r, c) -> (r - min_row + 1, c - min_col + 1)) game.board;
-        height = max_row - min_row + 1;
-        width  = max_col - min_col + 1}
-
+let make_board_from_set game set =
+    let arr = Array.make game.height 0 in
+    SqSet.iter
+        (fun (r, c) ->
+            arr.(r) <- arr.(r) lor (1 lsl c))
+        set;
+    arr
 
 let c_split game =
     if not (should_try_to_split game) then None else
 
-    let square = SqSet.choose game.board
+    let board_set = make_set_from_board game
     in
-    let region = !(pick_off_region game.board square (ref SqSet.empty))
+    let square = SqSet.choose board_set
     in
-    let region_size, game_size = SqSet.cardinal region, SqSet.cardinal game.board
+    let region = !(pick_off_region board_set square (ref SqSet.empty))
+    in
+    let region_size, game_size = SqSet.cardinal region, SqSet.cardinal board_set
     in
     if region_size == game_size
         then None
-        else let remains = SqSet.diff game.board region
+        else let remains = SqSet.diff board_set region
              in if region_size <= game_size - region_size
-                then Some (renormalize_game {game with board = region},
-                           renormalize_game {game with board = remains})
-                else Some (renormalize_game {game with board = remains},
-                           renormalize_game {game with board = region})
+                then Some ({game with board = make_board_from_set game region},
+                           {game with board = make_board_from_set game remains})
+                else Some ({game with board = make_board_from_set game remains},
+                           {game with board = make_board_from_set game region})
 
 
 let c_would_split game =
     if not (should_try_to_split game) then false else
 
-    let square = SqSet.choose game.board
+    let board_set = make_set_from_board game
     in
-    let region = !(pick_off_region game.board square (ref SqSet.empty))
+    let square = SqSet.choose board_set
     in
-    if SqSet.cardinal region == SqSet.cardinal game.board
+    let region = !(pick_off_region board_set square (ref SqSet.empty))
+    in
+    if SqSet.cardinal region == SqSet.cardinal board_set
         then false
         else true
 
 
-let min_breadth game =
-    let rows = Array.make (game.height+1) 0
-    and cols = Array.make (game.width+1) 0
-    in
-    SqSet.iter (fun (r, c) -> rows.(r) <- rows.(r) + 1; cols.(c) <- cols.(c) + 1) game.board;
-    let minimum = ref 1000 in
-    Array.iter (fun elt -> if elt > 0 then minimum := min !minimum elt) rows;
-    Array.iter (fun elt -> if elt > 0 then minimum := min !minimum elt) cols;
-    !minimum
-
 let calculate_center game =
     let (x_sum, y_sum, count) =
-        SqSet.fold (fun (a, b) (c, d, count) -> (a + c, b + d, count + 1))
-                   game.board
-                   (0, 0, 0)
+        List.fold_left (fun (a, b, count) (c, d) -> (a + c, b + d, count + 1))
+                       (0, 0, 0)
+                       (all_squares_on_board game)
     in
     if count = 0
         then (0, 0)
@@ -215,13 +230,13 @@ let pull_to_front fn xs =
     let ys, zs = List.partition fn xs
     in ys @ zs
 
-
+(*
 let is_row_or_column_missing game =
     let (r1, c1), (r2, c2) = get_last_move game
     in
     is_row_missing game r1 || (r1 <> r2 && is_row_missing game r2) ||
     is_column_missing game c1 || (c1 <> c2 && is_column_missing game c2)
-
+*)
 
 let c_sorted_options game =
     let opts = c_options_for_game game
@@ -232,16 +247,8 @@ let c_sorted_options game =
     in
     let y = List.map (fun (a, b) -> b) s
     in
-    (* pull_to_front is_row_or_column_missing y *)
-    (* shuffle opts *)
     pull_to_front (fun opt -> c_would_split opt) y
 
-let c_hasher game =
-    let arr = Array.make (game.height + 1) 0 in
-    SqSet.iter
-        (fun (r, c) -> arr.(r) <- arr.(r) lor (1 lsl c))
-        game.board;
-    arr
 
 let _ =
     let a = int_of_string Sys.argv.(1) and b = int_of_string Sys.argv.(2)
@@ -251,7 +258,7 @@ let _ =
                 else nimber_of_game
     in
     let (nimber, time) = with_time
-            (fun () -> fn (c_new_game a b) c_sorted_options c_split c_hasher)
+            (fun () -> fn (c_new_game a b) c_sorted_options c_split (fun x -> x.board))
     in
     Printf.printf "%d x %d: %d  (%.2f sec, %d positions, %d HT hits, %d splits)\n%!"
         a b nimber
